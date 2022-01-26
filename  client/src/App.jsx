a6be75a4d5728a4dcec5a7c80dda8008a6c3eed7
Manik Rana <manikrana54@gmail.com>
2022-01-26 08:51:13 -0500

    Add initial setup

---
+++  client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
