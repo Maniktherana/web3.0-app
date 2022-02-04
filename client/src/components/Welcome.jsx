85c040871f5205806ac3a9ebddac1d0765e4d290
Manik Rana <manikrana54@gmail.com>
2022-02-03 19:53:47 -0500

    Add card and form component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,10 +6,28 @@ import { Loader } from './'
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-lime-400 text-sm font-light text-white"
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+        placeholder={placeholder}
+        type={type}
+        step="0.001"
+        value={value}
+        onChange={(e) => handleChange(e(e, name))}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+)
+
 const Welcome = () => {
+
     const connectWallet = () => {
 
     }
+
+    const handleSubmit = () => {
+
+    }
+
+
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -68,6 +86,30 @@ const Welcome = () => {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+                            <Input placeholder="Anount (ETH)" name="amount" type="number" handleChange={() => {}} />
+                            <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={() => {}} />
+                            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+
+                            <div className="h-[1px] w-full bg-gray-400"/>
+
+                            {true ? (
+                                <Loader />
+                            ) : (
+                                <button
+                                    type="button"
+                                    onClick={handleSubmit}
+                                    className="text-white w-full mt-2 border-[1px] p-2 border-stone-300 rounded-full cursor-pointer"
+                                >
+                                    Send
+                                </button>
+                            )}
+                        </div>
+
+
+
                     </div>
 
                 </div>
