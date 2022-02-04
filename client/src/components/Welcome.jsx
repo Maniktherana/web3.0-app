02db26fc7df8f5243159607b4b6040b4f5becf50
Manik Rana <manikrana54@gmail.com>
2022-02-03 20:53:45 -0500

    Update grid color

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import { BsInfoCircle } from "react-icons/bs"
 
 import { Loader } from './'
 
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-lime-400 text-sm font-light text-white"
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
     <input
