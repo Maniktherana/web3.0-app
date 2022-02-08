6c2165f6402cc129f2fc3265273638b4891f8f6f
Manik Rana <manikrana54@gmail.com>
2022-02-07 19:48:53 -0500

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,25 @@
+import logo from '../../images/logo.png'
+
 const Footer = () => {
     return (
-        <h1>Footer</h1>
+       <div className="w-full flex md:justify center justify-between items-center flex-col p-4 gradient-bg-footer">
+           <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+               <div className="flex flex-[0.5] justify-center items-center">
+                    <img src={logo} alt="logo" className="w-32" />
+               </div>
+               <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+                    <p className="text-white text-base text-center mx-2 cursor-pointer ">Market</p>
+                    <p className="text-white text-base text-center mx-2 cursor-pointer ">Exchange</p>
+                    <p className="text-white text-base text-center mx-2 cursor-pointer ">Tutorials</p>
+                    <p className="text-white text-base text-center mx-2 cursor-pointer ">Wallets</p>
+
+               </div>
+           </div>
+           <div className="flex justify-center items-center flex-col mt-5">
+                <p className="text-white text-sm text-center">Come Join us</p>
+                <p className="text-white text-sm text-center">info@koin.com</p>
+           </div>
+       </div>
     )
 }
 
