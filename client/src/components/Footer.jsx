0a0fa9b2748dc24ad536bc0e95bf072ac33610b9
Manik Rana <manikrana54@gmail.com>
2022-02-07 19:53:01 -0500

    Update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -21,8 +21,8 @@ const Footer = () => {
             </div>
             <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5">
                 <div className="sm:w-[90%] w-full justify-between items-center mt-3">
-                <p className="text-white text-sm text-center">@Koin 2022</p>
-                <p className="text-white text-sm text-center">All rights reserved</p>
+                    <p className="text-white text-sm text-center">@Koin 2022</p>
+                    <p className="text-white text-sm text-center">All rights reserved</p>
                 </div>
             </div>
        </div>
