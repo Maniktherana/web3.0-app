68756f6438c0eef852c141e7f121faaf3219338d
Manik Rana <manikrana54@gmail.com>
2023-07-27 11:32:04 +0530

    change title to something fun

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -38,7 +38,7 @@ const Welcome = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Send Crypto <br /> across the world
+            I like eating food
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
